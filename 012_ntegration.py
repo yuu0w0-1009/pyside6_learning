49b241eefc96961a2b5dd4f36b8bf54d0985f9ed
yuu0w0-1009 <5322brian@gmail.com>
2026-01-06 23:59:11 +0800

    26/01/06

--- 012_ntegration.py
+++ 012_ntegration.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QPlainTextEdit, QLineEdit, QComboBox, QCheckBox
+from PySide6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, 
+                               QPushButton, QPlainTextEdit, QLineEdit, QComboBox, QCheckBox, 
+                               QButtonGroup, QRadioButton, QSlider)
 from PySide6.QtCore import Qt
 
 class MyWindow(QWidget):
@@ -7,39 +9,88 @@ class MyWindow(QWidget):
 
         self.btn1 = QPushButton()
         self.btn1.setText('按鈕')
-        self.btn1.clicked.connect(self.btn1clicked)
+        self.btnClear = QPushButton()
+        self.btnClear.setText('Clear')
+
         self.lbl = QLabel()
         self.lbl.setText('課程總結')
         self.lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
         self.textedit = QPlainTextEdit()
         self.textedit.setReadOnly(1)
+
         self.lineEdit = QLineEdit()
         self.lineEdit.setPlaceholderText('請輸入內容')
         self.lineEdit.setEchoMode(QLineEdit.EchoMode.Normal)
-        self.lineEdit.returnPressed.connect(self.ledtreturnPressed)
+        
         self.cbb = QComboBox()
         self.cbb.setPlaceholderText('請選擇')
         self.cbb.addItems(['a', 'b', 'c'])
-        self.cbb.currentTextChanged.connect(self.cbbIndexChanhed)
+        
         self.ckb1 = QCheckBox('#1')
         self.ckb1.setCheckable(1)
-        self.ckb1.stateChanged.connect(self.ckbChanged)
+        
         self.ckb2 = QCheckBox('#2')
         self.ckb2.setCheckable(1)
-        self.ckb2.stateChanged.connect(self.ckbChanged)
-        self.ckbTemp = 0
+        self.ckbTemp = [0]*2
+
+        self.groupEng = QButtonGroup()
+        self.rdbA = QRadioButton('A')
+        self.rdbB = QRadioButton('B')
+        self.groupEng.addButton(self.rdbA)
+        self.groupEng.addButton(self.rdbB)
+
+        self.groupNum = QButtonGroup()
+        self.rdb1 = QRadioButton('1')
+        self.rdb2 = QRadioButton('2')
+        self.groupNum.addButton(self.rdb1)
+        self.groupNum.addButton(self.rdb2)
 
         self.mainLayout = QVBoxLayout()
+
         self.ckbLayout = QHBoxLayout()
         self.ckbLayout.addWidget(self.ckb1)
         self.ckbLayout.addWidget(self.ckb2)
+
+        self.rbdEngLayout = QHBoxLayout()
+        self.rbdEngLayout.addWidget(self.rdbA)
+        self.rbdEngLayout.addWidget(self.rdbB)
+
+        self.rbdNumLayout = QHBoxLayout()
+        self.rbdNumLayout.addWidget(self.rdb1)
+        self.rbdNumLayout.addWidget(self.rdb2)
+
+        self.slider = QSlider(Qt.Orientation.Horizontal)
+        self.slider.setTickPosition(QSlider.TickPosition.TicksBothSides)
+        self.slider.setTickInterval(10)
+        self.slider.setRange(0,100)
+
         self.mainLayout.addWidget(self.lbl)
         self.mainLayout.addWidget(self.textedit)
         self.mainLayout.addWidget(self.lineEdit)
         self.mainLayout.addWidget(self.cbb)
         self.mainLayout.addLayout(self.ckbLayout)
+        self.mainLayout.addLayout(self.rbdEngLayout)
+        self.mainLayout.addLayout(self.rbdNumLayout)
+        self.mainLayout.addWidget(self.slider)
         self.mainLayout.addWidget(self.btn1)
+        self.mainLayout.addWidget(self.btnClear)
         self.setLayout(self.mainLayout)
+        self.bind()
+
+    def bind(self):
+        self.btn1.clicked.connect(self.btn1clicked)
+        self.btnClear.clicked.connect(lambda: self.textedit.clear())
+        self.lineEdit.returnPressed.connect(self.ledtreturnPressed)
+        self.cbb.currentTextChanged.connect(self.cbbIndexChanhed)
+        self.ckb1.stateChanged.connect(self.ckbChanged)
+        self.ckb2.stateChanged.connect(self.ckbChanged)
+        self.rdbA.clicked.connect(self.whichRdbClkedEng)
+        self.rdbB.clicked.connect(self.whichRdbClkedEng)
+        self.rdb1.clicked.connect(self.whichRdbClkedNum)
+        self.rdb2.clicked.connect(self.whichRdbClkedNum)
+        self.slider.valueChanged.connect(lambda: self.textedit.appendPlainText(f'{self.slider.value():d}'))
+
 
     def btn1clicked(self):
         self.textedit.appendPlainText('按鈕被點擊')
@@ -55,24 +106,25 @@ class MyWindow(QWidget):
     def ckbChanged(self):
         c1 = self.ckb1.isChecked()
         c2 = self.ckb2.isChecked()
-        if c1 == True and c2 == False and self.ckbTemp == 0:
+        if c1 == True and self.ckbTemp[0] == 0:
             self.textedit.appendPlainText(f'按下CheckBox1')
-            self.ckbTemp = 1
-        elif c2 == True and c1 == False and self.ckbTemp == 0:
+            self.ckbTemp[0] = 1
+        if c2 == True and self.ckbTemp[1] == 0:
             self.textedit.appendPlainText(f'按下CheckBox2')
-            self.ckbTemp = 2
-        if c1 == True and self.ckbTemp == 2:
-            self.textedit.appendPlainText(f'按下CheckBox1')
-            self.ckbTemp = 1
-            self.ckb2.setChecked(False)
-        elif c2 == True and self.ckbTemp == 1:
-            self.textedit.appendPlainText(f'按下CheckBox2')
-            self.ckbTemp = 2
-            self.ckb1.setChecked(False)
-        if c1 == False and self.ckbTemp == 1:
+            self.ckbTemp[1] = 1
+        if c1 == False and self.ckbTemp[0] == 1:
             self.textedit.appendPlainText(f'取消CheckBox1')
-        elif c2 == False and self.ckbTemp == 2:
+            self.ckbTemp[0] = 0
+        if c2 == False and self.ckbTemp[1] == 1:
             self.textedit.appendPlainText(f'取消CheckBox2')
+            self.ckbTemp[1] = 0
+
+    def whichRdbClkedEng(self):
+        self.textedit.appendPlainText(f'按下RadioButton {self.groupEng.checkedButton().text()} , 屬於groupEng')
+
+    def whichRdbClkedNum(self):
+        self.textedit.appendPlainText(f'按下RadioButton {self.groupNum.checkedButton().text()} , 屬於groupNum')
+
 
 if __name__ == '__main__':
     app = QApplication([])
